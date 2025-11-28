2d8b4cfec98cfbda77124c993ac7b1b4134b64be
joohone570 <kjhgy7738@gmail.com>
2025-11-28 13:18:39 +0900

    conflict test

--- average_calculate.py
+++ average_calculate.py
@@ -5,7 +5,7 @@ def average_calculate():
 		score = float(input("점수를 하나씩 입력(다 입력시 음수 입력):"))
 		if (score < 0):
 			average = sum / point
-			print("평균 점수:" average)
+			print("당신의 평균 점수:", average)
 			break
 		sum = sum + score
 		point = point + 1

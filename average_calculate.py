3c4bd08bea040d5e61574e484f4751e47cf69341
joohone570 <kjhgy7738@gmail.com>
2025-11-28 10:34:43 +0900

    Update average_calculate.py

--- average_calculate.py
+++ average_calculate.py
@@ -2,7 +2,7 @@ def average_calculate():
         sum = 0
         point = 0
         while True:
-                score = float(input("하나씩 점수를 입력하세요.(다 입력했다면 아무 음수나 입력하여 끝내):"))
+                score = float(input("하나씩 점수를 입력하세요.(다 입력했다면 아무 음수나 입력하여 끝내세요.):"))
                 if (score < 0):
                         average = sum / point
                         print("평균 점수:",average)
